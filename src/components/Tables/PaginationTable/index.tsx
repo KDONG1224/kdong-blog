6b30675dc3b8e0d0f872809d4732ff76e705a328
kdong <gkfl8809@naver.com>
2023-11-04 16:29:23 +0900

    fix

--- src/components/Tables/PaginationTable/index.tsx
+++ src/components/Tables/PaginationTable/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/ban-types */
 // base
 import React, { useMemo, useState, useEffect, useRef } from 'react';
